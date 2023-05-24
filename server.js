f30ac8d8891e121c2d2fd8da2f68c2fd50f7fcf0
Yusuf <87546692+YU5F@users.noreply.github.com>
2023-05-24 18:17:24 +0300

    Message insert to table feature
    API now can get sender key, reciever key, and a message as parameters and insert them into a table.
    If this table doesn't exist, it creates one and inserts the message.
    even if sender and reciever positions change and sender becomes reciever, app doesn't create a duplicate of that table and inserts the values into the existing one

--- server.js
+++ server.js
@@ -1,24 +1,79 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const db = require("./db/messages");
+const sqlite3 = require("sqlite3");
+const db = new sqlite3.Database("Messages.db");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.post("/messages", async (req, res) => {
-    const results = await db.insertMessage(req.body);
-    res.status(201).json({ id: results[0] });
-});
+    const { senderKey, recieverKey, message } = req.body;
+    const tableName = [senderKey, recieverKey].sort().join('_');
 
-app.get("/messages", async (req, res) => {
-    const messages = await db.getAllMessages();
-    res.status(200).json({ messages });
-});
+    const currentDate = new Date();
 
-app.patch("/messages/:id", async (req, res) => {
-    const id = await db.updateMessage(req.params.id, req.body);
-    res.status(200).json({ id });
+    // Format the date as "yyyy-mm-dd"
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+
+    // Format the time as "hh:mm"
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+
+    const date = `${year}-${month}-${day} ${hours}:${minutes}`;
+
+    db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name=?`, [tableName], (err, row) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json({ error: "Internal server error" });
+            return;
+        }
+
+        if (!row) {
+            db.run(
+                `CREATE TABLE IF NOT EXISTS ${tableName} (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    senderKey TEXT,
+                    receiverKey TEXT,
+                    message TEXT,
+                    sentDate TEXT
+                )`, (err) => {
+                if (err) {
+                    console.error(err);
+                    res.status(500).json({ error: "Internal server error" });
+                    return;
+                }
+
+                db.run(
+                    `INSERT INTO ${tableName} (senderKey, receiverKey, message, sentDate) VALUES (?, ?, ?, ?)`,
+                    [senderKey, recieverKey, message, date],
+                    (err) => {
+                        if (err) {
+                            console.error(err);
+                            res.status(500).json({ error: 'Internal server error' });
+                            return;
+                        }
+                        res.json({ message: `Message inserted successfully in table ${tableName}` });
+                    }
+                );
+            });
+        } else {
+            db.run(
+                `INSERT INTO ${tableName} (senderKey, receiverKey, message, sentDate) VALUES (?, ?, ?, ?)`,
+                [senderKey, recieverKey, message, date],
+                (err) => {
+                    if (err) {
+                        console.error(err);
+                        res.status(500).json({ error: 'Internal server error' });
+                        return;
+                    }
+                    res.json({ message: `Message inserted successfully in table ${tableName}` });
+                }
+            );
+        }
+    });
 });
 
 app.listen(1337, () => console.log("server is running on port 1337"));
