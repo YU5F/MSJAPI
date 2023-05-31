dbe74cb684b21427d0922cc27b811c6a2df1d5e5
Yusuf <87546692+YU5F@users.noreply.github.com>
2023-06-01 00:39:19 +0300

    get request made with query instead body

--- server.js
+++ server.js
@@ -3,104 +3,91 @@ const app = express();
 const bodyParser = require("body-parser");
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("Messages.db");
+const cors = require("cors");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
-app.post("/messages", async (req, res) => {
-    const { senderKey, recieverKey, message } = req.body;
-    const tableName = [senderKey, recieverKey].sort().join('_');
-
-    const currentDate = new Date();
-
-    // Format the date as "yyyy-mm-dd"
-    const year = currentDate.getFullYear();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate()).padStart(2, '0');
-
-    // Format the time as "hh:mm"
-    const hours = String(currentDate.getHours()).padStart(2, '0');
-    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
-
-    const date = `${year}-${month}-${day} ${hours}:${minutes}`;
-
+const createTableIfNotExists = (tableName, callback) => {
     db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name=?`, [tableName], (err, row) => {
         if (err) {
             console.error(err);
-            res.status(500).json({ error: "Internal server error" });
+            callback(err);
             return;
         }
 
         if (!row) {
+            // Table does not exist, create the table
             db.run(
                 `CREATE TABLE IF NOT EXISTS ${tableName} (
-                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    senderKey TEXT,
-                    receiverKey TEXT,
-                    message TEXT,
-                    sentDate TEXT
-                )`, (err) => {
-                if (err) {
-                    console.error(err);
-                    res.status(500).json({ error: "Internal server error" });
-                    return;
-                }
-
-                db.run(
-                    `INSERT INTO ${tableName} (senderKey, receiverKey, message, sentDate) VALUES (?, ?, ?, ?)`,
-                    [senderKey, recieverKey, message, date],
-                    (err) => {
-                        if (err) {
-                            console.error(err);
-                            res.status(500).json({ error: 'Internal server error' });
-                            return;
-                        }
-                        res.json({ message: `Message inserted successfully in table ${tableName}` });
-                    }
-                );
-            });
-        } else {
-            db.run(
-                `INSERT INTO ${tableName} (senderKey, receiverKey, message, sentDate) VALUES (?, ?, ?, ?)`,
-                [senderKey, recieverKey, message, date],
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            senderKey TEXT,
+            receiverKey TEXT,
+            message TEXT,
+            sentDate TEXT
+          )`,
                 (err) => {
                     if (err) {
                         console.error(err);
-                        res.status(500).json({ error: 'Internal server error' });
+                        callback(err);
                         return;
                     }
-                    res.json({ message: `Message inserted successfully in table ${tableName}` });
+                    callback(null);
                 }
             );
+        } else {
+            callback(null);
         }
+    });
+};
+
+// POST method for sending a message
+app.post('/messages', (req, res) => {
+    const { senderKey, receiverKey, message } = req.body;
+    const tableName = [senderKey, receiverKey].sort().join('_');
+    const currentDate = new Date().toLocaleString();
+
+    db.run(
+        `INSERT INTO ${tableName} (senderKey, receiverKey, message, sentDate) VALUES (?, ?, ?, ?)`,
+        [senderKey, receiverKey, message, currentDate],
+        (err) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Internal server error' });
+                return;
+            }
+            res.json({ message: `Message inserted successfully in table ${tableName}` });
+        }
+    );
+});
+
+// GET method for retrieving messages
+app.get('/messages', (req, res) => {
+    const { senderKey, receiverKey } = req.query;
+    const tableName = [senderKey, receiverKey].sort().join('_');
+
+    createTableIfNotExists(tableName, (err) => {
+        if (err) {
+            res.status(500).json({ error: 'Internal server error' });
+            return;
+        }
+
+        db.all(`SELECT * FROM ${tableName}`, (err, rows) => {
+            if (err) {
+                console.error(err);
+                res.status(500).json({ error: 'Internal server error' });
+                return;
+            }
+
+            if (rows.length === 0) {
+                res.json({ message: 'The chatting session has begun' });
+            } else {
+                res.json({ messages: rows });
+            }
+        });
     });
 });
 
-app.get("/messages", (req, res) => {
-    const { senderKey, recieverKey } = req.body;
-    const tableName = [senderKey, recieverKey].sort().join('_');
-  
-    db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name=?`, [tableName], (err, row) => {
-      if (err) {
-        console.error(err);
-        res.status(500).json({ error: "Internal server error" });
-        return;
-      }
-  
-      if (!row) {
-        // Table does not exist, return default value
-        res.json({ messages: [] });
-      } else {
-        db.all(`SELECT * FROM ${tableName}`, (err, rows) => {
-          if (err) {
-            console.error(err);
-            res.status(500).json({ error: "Internal server error" });
-            return;
-          }
-          res.json({ messages: rows });
-        });
-      }
-    });
-  });
 
 app.listen(1337, () => console.log("server is running on port 1337"));
